31762567d435e246df855b64a2709827ec4b3f90
lijie <541143520@qq.com>
2020-05-12 00:19:07 +0800

    两数字相加

---
+++ LeetCode/src/com/jack/leetcode/array/AddTwoNumbers.java
@@ -0,0 +1,65 @@
+package com.jack.leetcode.array;
+
+/**
+ * 给出两个 非空 的链表用来表示两个非负的整数。
+ * 其中，它们各自的位数是按照 逆序 的方式存储的，并且它们的每个节点只能存储一位数字。
+ * 如果，我们将这两个数相加起来，则会返回一个新的链表来表示它们的和。
+ * 您可以假设除了数字 0 之外，这两个数都不会以 0 开头。
+ * 示例：
+ * 输入：(2 -> 4 -> 3) + (5 -> 6 -> 4)
+ * 输出：7 -> 0 -> 8
+ * 原因：342 + 465 = 807
+ * 来源：力扣（LeetCode） 链接：https://leetcode-cn.com/problems/add-two-numbers
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ *
+ * @author CrazyJack
+ * @date 2020/05/11/21:03
+ */
+public class AddTwoNumbers {
+	static class ListNode {
+		int val;
+		ListNode next;
+
+		ListNode(int x) {
+			val = x;
+		}
+	}
+
+	public static void main(String[] args) {
+		ListNode listNode = new ListNode(2);
+		ListNode list2Node = new ListNode(4);
+		ListNode list3Node = new ListNode(3);
+		listNode.next = list2Node;
+		list2Node.next = list3Node;
+
+		ListNode list4Node = new ListNode(5);
+		ListNode list5Node = new ListNode(6);
+		ListNode list6Node = new ListNode(4);
+		list4Node.next = list5Node;
+		list5Node.next = list6Node;
+		ListNode listNode1 = addTwoNumbers(listNode, list4Node);
+		System.out.println(listNode1);
+
+	}
+
+
+	public static ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+		ListNode dummyHead = new ListNode(0);
+		ListNode p = l1, q = l2, curr = dummyHead;
+		int carry = 0;
+		while (p != null || q != null) {
+			int x = (p != null) ? p.val : 0;
+			int y = (q != null) ? q.val : 0;
+			int sum = carry + x + y;
+			carry = sum / 10;
+			curr.next = new ListNode(sum % 10);
+			curr = curr.next;
+			if (p != null) p = p.next;
+			if (q != null) q = q.next;
+		}
+		if (carry > 0) {
+			curr.next = new ListNode(carry);
+		}
+		return dummyHead.next;
+	}
+}
