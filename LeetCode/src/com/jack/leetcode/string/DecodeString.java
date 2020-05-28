3793c925fba4db675cf75a70cf0e0e0ddf5dc8d3
lijie <crazyjie262@gmail.com>
2020-05-28 13:59:00 +0800

    递归转码

---
+++ LeetCode/src/com/jack/leetcode/string/DecodeString.java
@@ -0,0 +1,87 @@
+package com.jack.leetcode.string;
+
+/**
+ * 给定一个经过编码的字符串，返回它解码后的字符串。
+ * <p>
+ * 编码规则为: k[encoded_string]，表示其中方括号内部的 encoded_string 正好重复 k 次。注意 k 保证为正整数。
+ * <p>
+ * 你可以认为输入字符串总是有效的；输入字符串中没有额外的空格，且输入的方括号总是符合格式要求的。
+ * <p>
+ * 此外，你可以认为原始数据不包含数字，所有的数字只表示重复的次数 k ，例如不会出现像 3a 或 2[4] 的输入。
+ * <p>
+ * <p>
+ * s = "3[a]2[bc]", 返回 "aaabcbc".
+ * s = "3[a2[c]]", 返回 "accaccacc".
+ * s = "2[abc]3[cd]ef", 返回 "abcabccdcdcdef".
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/decode-string
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+public class DecodeString {
+    String src;
+    int ptr;
+
+    public String decodeString(String s) {
+        src = s;
+        ptr = 0;
+        return getString();
+    }
+
+    public String getString() {
+        if (ptr == src.length() || src.charAt(ptr) == ']') {
+            // String -> EPS
+            return "";
+        }
+
+        char cur = src.charAt(ptr);
+        int repTime = 1;
+        String ret = "";
+
+        if (Character.isDigit(cur)) {
+            // String -> Digits [ String ] String
+            // 解析 Digits
+            repTime = getDigits();
+            // 过滤左括号
+            ++ptr;
+            // 解析 String
+            String str = getString();
+            // 过滤右括号
+            ++ptr;
+            // 构造字符串
+            while (repTime-- > 0) {
+                ret += str;
+            }
+        } else if (Character.isLetter(cur)) {
+            // String -> Char String
+            // 解析 Char
+            ret = String.valueOf(src.charAt(ptr++));
+        }
+
+        return ret + getString();
+    }
+
+    public int getDigits() {
+        char c1 = src.charAt(ptr);
+        System.out.println("my c1 --> " + Integer.valueOf(String.valueOf(c1)));
+        int ret = 0;
+        while (ptr < src.length() && Character.isDigit(src.charAt(ptr))) {
+            int temp = ret * 10;
+            System.out.println("temp-->" + temp);
+            char c = src.charAt(ptr++);
+            System.out.println("c --> " + c);
+            int retTemp = temp + c;
+            ret = retTemp - '0';
+            System.out.println("retTemp -->" + retTemp);
+            System.out.println("ret --> " + ret);
+        }
+        return ret;
+    }
+
+    public static void main(String[] args) {
+        String s = "3[a2[c]]";
+        String b = "3[a]2[bc]";
+        DecodeString decodeString = new DecodeString();
+        System.out.println(decodeString.decodeString(b));
+    }
+}
