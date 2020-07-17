0bade0f3f585f275ae505ae7eda13951bcf4f720
lijie <crazyjie262@gmail.com>
2020-07-17 11:51:35 +0800

    排序数组插入

---
+++ LeetCode/src/com/jack/leetcode/array/SearchInsert.java
@@ -0,0 +1,51 @@
+package com.jack.leetcode.array;
+
+/**
+ * 给定一个排序数组和一个目标值，在数组中找到目标值，并返回其索引。如果目标值不存在于数组中，返回它将会被按顺序插入的位置。
+ * <p>
+ * 你可以假设数组中无重复元素。
+ * <p>
+ * 示例 1:
+ * <p>
+ * 输入: [1,3,5,6], 5
+ * 输出: 2
+ * 示例 2:
+ * <p>
+ * 输入: [1,3,5,6], 2
+ * 输出: 1
+ * 示例 3:
+ * <p>
+ * 输入: [1,3,5,6], 7
+ * 输出: 4
+ * 示例 4:
+ * <p>
+ * 输入: [1,3,5,6], 0
+ * 输出: 0
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/search-insert-position
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ *
+ * @author crazyjack262
+ * @date 2020-07-17 09:29
+ */
+public class SearchInsert {
+
+    public static int searchInsert(int[] nums, int target) {
+        if (nums[0] >= target) {
+            return 0;
+        }
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i] >= target) {
+                return i;
+            }
+        }
+        return nums.length;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {1, 3, 5, 6};
+        System.out.println(searchInsert(nums, 2));
+
+    }
+}
